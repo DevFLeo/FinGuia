8b944883204e0a4ce987bae365fcdb46aefaf102
lucas.bastos.rezende2 <lucas.bastos.rezende2@gmail.com>
2026-04-14 00:41:31 -0400

    Fix: Corrige ícones ic_dashboard e ic_palette e atualiza dependências e configurações de build.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.finguia"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.finguia"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -28,11 +28,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         compose = true
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 // Arquivo de build raiz onde você pode adicionar opções de configuração comuns a todos os submódulos. NÃO MUDAR
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
